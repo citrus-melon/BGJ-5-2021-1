48943c73b6ad63c510a554de946331f9313dfe02
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-02-19 08:29:02 -0500

    make diagonal movement zigzagged

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform moveTarget;
     public LayerMask collidesWith;
-    // Start is called before the first frame update
+    private bool diagonalHorizontal = false;
     void Start()
     {
         
@@ -21,13 +21,29 @@ public class PlayerMovement : MonoBehaviour
 
         if (Vector3.Distance(transform.position, moveTarget.position) > .05f) return;
 
-        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-            Vector3 newTarget = moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-            if (!Physics2D.OverlapPoint(newTarget, collidesWith)) moveTarget.position = newTarget;
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+            if (diagonalHorizontal) {
+                MoveHorizontal();
+            } else {
+                MoveVertical();
+            }
+            diagonalHorizontal = !diagonalHorizontal;
         }
-        if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-            Vector3 newTarget = moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-            if (!Physics2D.OverlapPoint(newTarget, collidesWith)) moveTarget.position = newTarget;
+        else if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+            MoveHorizontal();
         }
+        else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+            MoveVertical();
+        }
+    }
+
+    void MoveVertical() {
+        Vector3 newTarget = moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        if (!Physics2D.OverlapPoint(newTarget, collidesWith)) moveTarget.position = newTarget;
+    }
+
+    void MoveHorizontal() {
+        Vector3 newTarget = moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        if (!Physics2D.OverlapPoint(newTarget, collidesWith)) moveTarget.position = newTarget;
     }
 }
