623409446f5ce49770ec5890133fe0f5012b3f37
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-02-21 16:03:12 -0500

    Add SFX Manager/Player to Every Scene
    This allows you to play scenes in the editor, as previously the player movement script would attempt to call the play sfx function and cause an exception since the sfxmanager was not in the scene.

--- Assets/Scripts/SfxPlayer.cs
+++ Assets/Scripts/SfxPlayer.cs
@@ -13,12 +13,6 @@ public class SfxPlayer : MonoBehaviour
     private int soundID = 0;
     void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("sfx"); // find all musics
-        if (objs.Length > 1) // if the list of musics is more than myself (aka theres already a music)
-        {
-            Destroy(this.gameObject); // delete myself because theres already another music
-        }
-        DontDestroyOnLoad(this.gameObject); // otherwise im the first music, and I should stay with the player forever!
         singleton = this;
     }
     public void queueSound(int sound) {
